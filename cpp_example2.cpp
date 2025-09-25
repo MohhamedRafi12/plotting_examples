5eab2c5339327e2894ae75ecff21efa5969948c5
fxc7ss <fxc7ss@virigina.edu>
2025-09-24 20:09:32 -0400

    update

--- cpp_example2.cpp
+++ cpp_example2.cpp
@@ -6,8 +6,8 @@
 
 #include "TApplication.h"
 #include "TROOT.h"
-#include "TH1F.h"
-#include "TF1.h"
+#include "TH2F.h"
+#include "TF2.h"
 #include "TCanvas.h"
 #include "TStyle.h"
 #include "TRandom3.h"
@@ -19,80 +19,60 @@ using namespace std;
 using namespace ROOT::Math;
 
 
-// examples of various plots in C++ program
-void cpp_example(int samples=10000){
+void cpp_example2(int samples=10000){
   // gStyle->SetOptStat(0);  // turn off default stats box in histograms
 
-  // Histogram filled with a normal distribution
   auto tr = new TRandom3();
-  auto hist1 = new TH1F("hist1","random gauss;x;frequency",100,50,150);
-  auto fpeak = new TF1("fpeak","exp(-0.5*(x-[0])*(x-[0])/[1]/[1])",50,150);
+
+  auto hist1 = new TH2F("hist1","random gauss 2D;x;y",100,50,150,100,50,150);
+  auto fpeak = new TF2("fpeak", "exp(-0.5*((x-[0])*(x-[0])/[1]/[1] + (y-[0])*(y-[0])/[1]/[1]))", 50,150,50,150);
   fpeak->SetParameters(100,6);
   hist1->FillRandom("fpeak",samples);
-  auto tc1 = new TCanvas("c1","Canvas1");
-  hist1->Draw("e");
-  tc1->Update();
 
-  // A multi panel plot
-  auto tc2=new TCanvas("c2","Canvas2");
-  tc2->Divide(2,2);  // divide in to 2x2 panels
+  auto tc2 = new TCanvas("c2","Canvas2");
+  tc2->Divide(2,2);
   tc2->cd(1);
-  hist1->Draw("e");
+  hist1->Draw("COLZ");
 
-  // add a random uniform offset to the histogram
-  auto hist2 = (TH1F*) hist1->Clone("hist2");
-  hist2->SetTitle("Gauss+offset;x;frequency");
+  auto hist2 = (TH2F*) hist1->Clone("hist2");
+  hist2->SetTitle("Gauss+offset 2D;x;y");
   for (int i=0; i<samples/3; ++i){
-    hist2->Fill(tr->Uniform(50,150));
+    double xu = tr->Uniform(50,150);
+    double yu = tr->Uniform(50,150);
+    hist2->Fill(xu,yu);
   }
   tc2->cd(2);
-  hist2->Draw("e");
+  hist2->Draw("COLZ");
 
-  // apply an offset to give us a 1/x^2 baseline
-  auto hist3 = (TH1F*) hist1->Clone("hist3");
-  hist3->SetTitle("Gauss+offset2;x;frequency");
-  auto base2 = new TF1("base2","1/x/x",1,11);
+  auto hist3 = (TH2F*) hist1->Clone("hist3");
+  hist3->SetTitle("Gauss+offset2 (1/x^{2}) 2D;x;y");
+  auto base2 = new TF1("base2","1/x/x",1,10);
   for (int i=0; i<samples*30; ++i){
     double x = base2->GetRandom()*10+40;
-    hist3->Fill(x);
+    double y = base2->GetRandom()*10+40;
+    hist3->Fill(x,y);
   }
-  tc2->cd(3)->SetLogy();
-  hist3->Draw("e");
+  auto pad3 = tc2->cd(3);
+  pad3->SetLogz();
+  hist3->Draw("COLZ");
 
-  // a double gaussian
-  auto hist4 = (TH1F*) hist1->Clone("hist4");
-  hist4->SetTitle("Double Gaussian;x;frequency");
+  auto hist4 = (TH2F*) hist1->Clone("hist4");
+  hist4->SetTitle("Double Gaussian 2D;x;y");
   fpeak->SetParameter(1,20);
   hist4->FillRandom("fpeak",samples/2);
   tc2->cd(4);
-  hist4->Draw("e");
+  hist4->Draw("COLZ");
 
-  tc2->Update();
-
-  // example for saving our plot outputs in different formats
-  tc1->SaveAs("canvas1.png");
-  tc2->SaveAs("canvas2.pdf");
-  
+  tc2->SaveAs("canvas2d_cpp.png");
 }
 
 int main(int argc, char **argv) {
-  int nsamples=10000;  // set sample sizes
-
-  // This allows you to view ROOT-based graphics in your C++ program
-  // If you don't want view graphics (eg just read/process/write data files), 
-  // this can be ignored
-  TApplication theApp("App", &argc, argv);
-
+  int nsamples=10000;
   if (argc>1) nsamples=atoi(argv[1]);
-  
-  cpp_example(nsamples);
 
-  // view graphics in ROOT if we are in an interactive session
-  if (!gROOT->IsBatch()) {
-      cout << "To exit, quit ROOT from the File menu of the plot (or use control-C)" << endl;
-    theApp.SetIdleTimer(30,".q"); // set up a failsafe timer to end the program
-    theApp.Run(true);
-  }
+
+  gROOT->SetBatch(true);
+  cpp_example2(nsamples);
+
   return 0;
 }
-  
